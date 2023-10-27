318b0b51cd11b5cf5e4296bdeb65c6303c6d73ad
vicbguti <vicbguti@espol.edu.ec>
2023-10-26 22:23:44 -0500

    Communications package update

--- transactions/src/main/java/communications/Notificable.java
+++ transactions/src/main/java/communications/Notificable.java
@@ -1,17 +1,7 @@
 package communications;
 
-
 import payment.Pago;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
-
-/**
- *
- * @author vicbguti
- */
 public interface Notificable {
-    public void notificar(Pago pago );
+    public void notificar(Pago pago);
 }
--- transactions/src/main/java/payment/Pago.java
+++ transactions/src/main/java/payment/Pago.java
@@ -2,15 +2,6 @@ package payment;
 
 import communications.Session;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author vicbguti
- */
 public abstract class Pago {
     Session session;
 }
